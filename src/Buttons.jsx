c04052121aa87ebb95fc59607de43feaa0d106cd
OksanaNova <o.nova0804@gmail.com>
2024-12-11 20:06:32 -0700

    add button all products

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,6 +1,7 @@
-function Buttons({filterClothes}) {
+function Buttons({filterClothes, allClothes}) {
     return (
         <div className="cont">
+            <button onClick={() => allClothes()}>All products</button>
             <button className="change" onClick={() => filterClothes("dress")} >Dresses</button>
             <button className="change" onClick={() => filterClothes("pants")} >Pants</button>
             <button className="change" onClick={() => filterClothes("skirt")} >Skirts</button>
--- src/Home.jsx
+++ src/Home.jsx
@@ -10,13 +10,17 @@ function Home() {
       const newClothes = data.filter(element => element.searchTerm === searchKey);
       setClothes(newClothes);
     }
+
+    const showAll = () => {
+      setClothes(data);
+    }
   
     return (
       <div>
         <div className='cont'>
           <h2 className='back'>Free Standart Shipping</h2>
         </div>
-        <Buttons filterClothes={choseClothes}/>
+        <Buttons filterClothes={choseClothes} allClothes={showAll}/>
         <Clothes itemsForSale={clothes}/>
       </div>
     )
